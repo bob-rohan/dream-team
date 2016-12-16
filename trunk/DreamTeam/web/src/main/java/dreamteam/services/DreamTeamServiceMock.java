66db7ad8476b1118501141bb7b3a545eac613b37
Bob Rohan <bob.rohan87@gmail.com>
2016-12-16 01:31:35 +0000

    Replacing spring XML configuration with spring boot annotated java annotated configuration.
    As a result of this change spring framework moved from version 3.1.1 to 4.3.3.
    
    Lastly added some mocking of the service class to provide quick response in manual tests. Configuration currently manual via qualified bean.

---
+++ trunk/DreamTeam/web/src/main/java/dreamteam/services/DreamTeamServiceMock.java
@@ -0,0 +1,50 @@
+package dreamteam.services;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import dreamteam.domain.Goalkeeper;
+import dreamteam.domain.Player;
+import dreamteam.web.DreamTeamController;
+
+@Service
+public class DreamTeamServiceMock implements DreamTeamService{
+	
+	private final static Logger LOGGER = Logger.getLogger(DreamTeamController.class);
+	
+	private Collection<Player> players;
+	
+	public Collection<Player> getPlayers(){
+		
+		if (players == null){
+			generatePlayers();
+		}
+		
+		return players;
+			
+	}
+	
+	// TODO: Refactor - SRP violation
+	private void generatePlayers(){
+		players = new ArrayList<Player>();
+
+		players.add(getPlayer());
+		players.add(getPlayer());
+		players.add(getPlayer());
+	}
+	
+	private Player getPlayer(){
+		Goalkeeper goalkeeper = new Goalkeeper();
+		goalkeeper.setVisibility(true);
+		goalkeeper.setId(0);
+		goalkeeper.setName("One");
+		goalkeeper.setNowCost(new BigDecimal(1.7f));
+		goalkeeper.setTotalPoints(38);
+		return goalkeeper;
+	}
+		
+}
--- trunk/DreamTeam/web/src/main/java/dreamteam/web/DreamTeamController.java
+++ trunk/DreamTeam/web/src/main/java/dreamteam/web/DreamTeamController.java
@@ -2,12 +2,10 @@ package dreamteam.web;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +25,7 @@ public class DreamTeamController {
 	private final static String HOME_VIEW = "home";
 	
 	@Autowired
+	@Qualifier("dreamTeamServiceImpl")
 	private DreamTeamService dreamTeamService;
 	
 	@RequestMapping("/home")
