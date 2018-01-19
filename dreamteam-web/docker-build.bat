e71d6f02e10c0c2db6e1a6294c9dd46ca78701d5
Bob Rohan <bob.rohan87@gmail.com>
2018-01-19 22:40:15 +0000

    Add Jenkinsfile for pipeline #10

--- dreamteam-web/docker-build.bat
+++ dreamteam-web/docker-build.bat
@@ -1,4 +1,4 @@
 docker-machine env default;
-@FOR /f "tokens=*" %i IN ('docker-machine env default') DO %i;
+`@FOR /f "tokens=*" %i IN ('docker-machine env default') DO %i`
 cd dreamteam-web
 docker build -t bobrohan/dreamteam-web:latest .
