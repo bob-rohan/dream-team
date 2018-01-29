7c16d3d88d03a22d2b1c6f7b876c90c556c72371
Bob Rohan <bob.rohan87@gmail.com>
2018-01-29 20:49:27 +0000

    schedule refresh player job #20

---
+++ services/src/main/java/rohan/dreamteam/services/tasks/RefreshPlayersTask.java
@@ -0,0 +1,27 @@
+package rohan.dreamteam.services.tasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import rohan.dreamteam.services.DreamTeamService;
+
+@Service
+@Profile("!mock")
+public class RefreshPlayersTask {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RefreshPlayersTask.class);
+
+	@Autowired
+	private DreamTeamService dreamTeamService;
+
+	@Scheduled(fixedRateString = "${refreshPlayers.fixedRate}")
+	public void refreshPlayers() {
+		LOGGER.info("Starting refresh players task");
+
+		dreamTeamService.refreshPlayers();
+	}
+}
